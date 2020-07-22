0d1152f01032848a9151ebffd642a11ec74d7e44
Kaiwalya Wategaonkar <wkaiwalya@Kaiwalyas-MBP.broadband>
2020-07-22 14:22:26 +0100

    Final code

--- todo_app/app.py
+++ todo_app/app.py
@@ -6,9 +6,39 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello World!'
+    
+    if request.method == 'POST':
+        item_title = request.form['Title']
+        session.add_item(item_title)
+        return redirect('/')
+    else:
+        items_list = session.get_items()
+        # print(items_list)
+        return render_template('index.html', items_list=items_list)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    print('id', id)
+    session.delete_item(id)
+    return redirect('/')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    k = session.get_item(id)
+
+    if request.method == 'POST':
+        
+        k['status'] = request.form['Status']
+        k['title'] = request.form['Title']
+        session.save_item(k)
+        return redirect('/')
+
+    else:
+        
+        # print(items_list)
+        return render_template('update.html', k=k)
 
 
 if __name__ == '__main__':
